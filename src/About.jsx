a859101e9a4ac849349f69326bcc8def254a6483
qkb2 <53836634+qkb2@users.noreply.github.com>
2024-01-02 16:36:56 +0100

    Small fix to about page

--- src/About.jsx
+++ src/About.jsx
@@ -9,12 +9,13 @@ class About extends React.Component {
                 <header className='App-header'>
                     <h2>About Page</h2>
                     <p>
-                        I'm a 2nd year student at PUT in Poznań. My interests include microcotrollers etc.
+                        I'm a 3rd year student at PUT in Poznań. My interests include microcontrollers etc.
                         Me and my friend even constructed a working line follower robot with our own PCB using Atmega8 once
                         - I can provide videos if requested.
+                        I also constructed a breathalyzer with a small for IoT service attached (will provide videos if requested).
                         I meddle in C if needed, know a little bit of statistics and data analysis - Pandas included, can perform
-                        rudimentary operations in IOS for Cisco devices, would settle to work with most technologies
-                        if given a chance.
+                        rudimentary operations in IOS for Cisco devices, would settle to work with most technologies.
+                        I know some Java - enough to build basic backends with Spring.
                     </p>
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/tWV4MuFYxNM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 </header>
